9b00983f408697c5bff290180d5dcd9afa78a26d
Sebastian Bolaños <sbolanosm.codyd@gmail.com>
2023-03-09 14:32:31 -0500

    perf: genera doc registros nuevos v2

--- compara_registros.py
+++ compara_registros.py
@@ -16,11 +16,21 @@ counts = df_all.apply(tuple, axis=1).value_counts()
 # Filtra el DataFrame para mostrar solo las filas que aparecen una vez
 df_unique = df_all[df_all.apply(tuple, axis=1).isin(counts[counts == 1].index)]
 
+# Selecciona solo las columnas que se encuentran el archivo
+df_unique = df_unique.loc[:, ['id','name', 'email', 'category_id/name', 'company_type']]
+
 # Muestra el resultado
-print(df_unique)
+print("\n---------------------------Registros nuevos de la nueva db CalzadoTriunfo---------------------------\n\n",df_unique)
 print("\n")
 num_new_records = len(df_unique)
 print("Número de registros nuevos:", num_new_records)
+
+# Si quieres rellenar los valores vacíos de la columna "company_type" con el valor "Individual"
+df_unique['company_type'].fillna('Individual', inplace=True)
+
+# Elimino la columna para no agregarla a mi archivo final
+# Elimina la columna 'id'
+df_unique = df_unique.drop('id', axis=1)
 
 #me crea un archivo en excel con los registros nuevos
 df_unique.to_excel('archivos_excel/semana_6-12-Marzo/archivo_importar_nuevo/registros_nuevos.xlsx', index=False)
