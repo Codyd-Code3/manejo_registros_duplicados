81c30ff0ce49275afea9fb4f49697054b95f0096
Sebastian Bolaños <sbolanosm.codyd@gmail.com>
2023-03-09 12:06:10 -0500

    feat: genera un excel con registros nuevos

--- compara_registros.py
+++ compara_registros.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Lee los dos archivos de Excel y los convierte en DataFrames
-df1 = pd.read_excel('archivos_excel/db_semana_3.xlsx')
-df2 = pd.read_excel('archivos_excel/db_semana_4.xlsx')
+df1 = pd.read_excel('archivos_excel/semana_6-12-Marzo/db_en_odoo_vieja.xlsx')
+df2 = pd.read_excel('archivos_excel/semana_6-12-Marzo/db_nata_nueva.xlsx')
 
 # Combina los dos DataFrames en uno solo
 df_all = pd.concat([df1, df2])
 
 # Elimina los duplicados basados en la columna 'correo'
-df_all.drop_duplicates(subset=['Correo electrónico'], keep=False, inplace=True)
+df_all.drop_duplicates(subset=['email'], keep=False, inplace=True)
 
 # Cuenta cuántas veces aparece cada fila en el DataFrame combinado
 counts = df_all.apply(tuple, axis=1).value_counts()
@@ -21,3 +21,6 @@ print(df_unique)
 print("\n")
 num_new_records = len(df_unique)
 print("Número de registros nuevos:", num_new_records)
+
+#me crea un archivo en excel con los registros nuevos
+df_unique.to_excel('archivos_excel/semana_6-12-Marzo/archivo_importar_nuevo/registros_nuevos.xlsx', index=False)
